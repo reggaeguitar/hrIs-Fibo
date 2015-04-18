aa53483dc06581ef4ad76fb822bfe5f473805b80
Joseph Brady <escalade2005@hotmail.com>
2015-04-18 12:28:35 -0500

    Initial commit. Getting WA for larger numbers

---
+++ hrIs-Fibo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace hrIs_Fibo
+{
+    public class Solution
+    {
+        const double Eps = 1e-5;
+
+        static void Main()
+        {
+            int numCases = Int32.Parse(Console.ReadLine());
+            while (numCases-- > 0)
+            {
+                Console.WriteLine(Solve(Console.ReadLine()));
+            }
+        }
+
+        public static string Solve(string input)
+        {
+            bool isFibo = false;
+            long num = Int64.Parse(input);
+            long a = 5 * num * num;
+            long plusFour = a + 4;
+            var plusFourSqrtDbl = Math.Sqrt(plusFour);
+            var plusFourSqrtInt = (long)plusFourSqrtDbl;
+            if (plusFourSqrtDbl - plusFourSqrtInt < Eps)
+            {
+                isFibo = true;
+            }
+            if (!isFibo)
+            {
+                long minusFour = a - 4;
+                var minusFourSqrtDbl = Math.Sqrt(minusFour);
+                var minusFourSqrtInt = (long)minusFourSqrtDbl;
+                if (minusFourSqrtDbl - minusFourSqrtInt < Eps)
+                {
+                    isFibo = true;
+                }
+            }
+            return isFibo ? "IsFibo" : "IsNotFibo";
+        }
+    }
+}
---
+++ hrIs-FiboTest/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.IO;
+using hrIs_Fibo;
+
+namespace hrIs_FiboTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inputs = File.ReadAllLines("input6.txt");
+            var outputs = File.ReadAllLines("output6.txt");
+            for (int index = 1; index < inputs.Length; index++)
+            {
+                var input = inputs[index];
+                var output = Solution.Solve(input);
+                if (output != outputs[index - 1])
+                {
+                    Console.WriteLine("");
+                }
+            }
+        }
+    }
+}
